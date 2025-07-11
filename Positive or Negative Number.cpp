6089ded35740b65b736443426bcd10d7ac9f1fb1
tanmayambudkar <tanmayambudkar@gmail.com>
2025-07-11 11:43:46 +0530

    Add files via upload

---
+++ Positive or Negative Number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter a Number: ";
+    cin>>a;
+    if(a>0){
+        cout<<"The Number is Positive";
+    }
+    else{
+        cout<<"The Number is Negative";
+    }
+    return 0;
+}
